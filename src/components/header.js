1c874bfc67ad74e8bcc78f1111897b3968d5c7db
Giusy <gc@datespotz.com>
2023-10-15 09:52:41 -0400

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import {AppBar, Toolbar, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SideDrawer from './sideDrawer.js'
 
 const Header = () => {
     return (
-        <>
-         header
-        </>
+        <AppBar
+            position="fixed"
+            style={{
+                backgroundColor: '#1a3640',
+                boxshadow: 'none',
+                padding:'20px 0px'
+            }}
+        >
+            <Toolbar>
+                <div classname='header_logo'>
+                    <div className="font_pixelify header_logo_idio">Idioteque</div>
+                </div>
+                <IconButton
+                    aria-label='Menu'
+                    color='inherit'
+
+                >    
+                    <MenuIcon/>
+                </IconButton>
+                
+                <SideDrawer/>
+
+
+            </Toolbar>
+         
+        </AppBar>
     )
 }
 
---
+++ src/components/sideDrawer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Drawer, List, ListItem } from '@mui/material'
+
+
+const SideDrawer = (props) => {
+    return(
+        <>
+        </>
+    )
+
+}
+
+export default SideDrawer;
